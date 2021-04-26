3f5a226e36d12a352aafaed4757554bfa2bcfc24
tashfiq333 <tashfiqnahiyan333@gmail.com>
2021-04-27 01:47:03 +0600

    -added  Registration complete page

---
+++ src/pages/Registration-complete/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../../pages/SIgnIn/SignIn.css";
+
+const RegistrationComplete = () => {
+  return (
+    <div className="background-signup">
+      <section id="signIn-section">
+        <div className="sample">
+          <div className="signIn-box signIn-box-medium signIn-box-small">
+            <h2>REGISTRATION COMPLETE!</h2>
+            <p>
+              We have received your registration form. If you are not redirected
+              within 5 seconds, press the button below.
+            </p>
+
+            <form>
+              <div>
+                <button className=" btn btn-type1">CONFIRM</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default RegistrationComplete;
